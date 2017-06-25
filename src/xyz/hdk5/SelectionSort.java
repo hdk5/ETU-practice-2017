d822295660e9b1be94e6a2dce4b03dc2010261f8
hdk5 <humandk55@gmail.com>
2017-06-25 17:11:13 +0300

    GUI demonstration completed

--- src/xyz/hdk5/SelectionSort.java
+++ src/xyz/hdk5/SelectionSort.java
@@ -13,15 +13,19 @@ public class SelectionSort extends JFrame {
     private int sortedIndex = -1;
     private int minIndex = -1;
     private int currIndex = -1;
+    private boolean animation = false;
+
     //Элементы графического интерфейса
     private JLabel numLabel;
     private JPanel drawPanel;
+    private JLabel manualInputLabel;
     private JSpinner numOfElements;
     private JButton randButton;
     private JButton stepButton;
     private JButton animationButton;
-
-    private boolean animation = false;
+    private JLabel explanationLabel;
+    private JButton manualCommitButton;
+    private JTextField manualInputField;
 
     public SelectionSort() {
         super("Selection Sort Algorithm Visualisation");
@@ -30,12 +34,12 @@ public class SelectionSort extends JFrame {
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
         c.insets = new Insets(3, 3, 3, 3);
+
         //Строка кол-ва элементов
         numLabel = new JLabel("Number of elements: ");
         c.gridx = 0;
         c.gridy = 0;
         add(numLabel, c);
-
 
         //Количество элемент(прокрутка)
         numOfElements = new JSpinner(
@@ -43,7 +47,9 @@ public class SelectionSort extends JFrame {
         );
         c.gridx = 1;
         c.gridy = 0;
+        c.gridwidth = 2;
         add(numOfElements, c);
+        c.gridwidth = 1;
 
         //Кнопка рандомизации элементов(генерации)
         randButton = new JButton("Randomize");
@@ -56,35 +62,66 @@ public class SelectionSort extends JFrame {
             randomize((Integer) numOfElements.getValue());
             drawPanel.repaint();
         });
-
-        c.gridx = 2;
+        c.gridx = 4;
         c.gridy = 0;
         add(randButton, c);
+
+
+        manualInputLabel = new JLabel("Manual array input: ");
+        c.gridx = 0;
+        c.gridy = 1;
+        add(manualInputLabel, c);
+
+        manualInputField = new JTextField();
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        add(manualInputField, c);
+        c.gridwidth = 1;
+
+        manualCommitButton = new JButton("Commit");
+        manualCommitButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this,
+                    "Can't parse input string",
+                    "Parse error",
+                    JOptionPane.ERROR_MESSAGE);
+        });
+        c.gridx = 4;
+        c.gridy = 1;
+        add(manualCommitButton, c);
+
         //Кнопка выполнения шага сортировки
         stepButton = new JButton("Step");
         stepButton.addActionListener(e -> {
             Collections.sort(array);
             drawPanel.repaint();
         });
-        c.gridx = 3;
-        c.gridy = 0;
+        c.gridx = 0;
+        c.gridy = 3;
         add(stepButton, c);
-        //Кнопка отключения показа анимации
-        animationButton = new JButton("Start/stop animation");
-        animationButton.addActionListener(e -> {
-
-        });
-        c.gridx = 4;
-        c.gridy = 0;
-        add(animationButton, c);
 
         drawPanel = new DrawArrayPanel();
         drawPanel.setPreferredSize(new Dimension(800, 300));
         drawPanel.setVisible(true);
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         c.gridwidth = 6;
         add(drawPanel, c);
+        c.gridwidth = 1;
+
+        //Кнопка отключения показа анимации
+        animationButton = new JButton("Start/stop animation");
+        animationButton.addActionListener(e -> {
+
+        });
+        c.gridx = 1;
+        c.gridy = 3;
+        add(animationButton, c);
+
+        explanationLabel = new JLabel("Define array to begin.");
+        c.gridx = 2;
+        c.gridy = 3;
+        add(explanationLabel, c);
 
         pack();
         setVisible(true);
@@ -94,6 +131,7 @@ public class SelectionSort extends JFrame {
     public static void main(String[] args) {
         SelectionSort mainWindow = new SelectionSort();
     }
+
     //Функция перемешивания элементов
     private void randomize(int size) {
         array = new ArrayList<Integer>();
@@ -102,6 +140,7 @@ public class SelectionSort extends JFrame {
         }
         Collections.shuffle(array);
     }
+
     //Визуализация массива в виде столбчатой диаграммы
     private class DrawArrayPanel extends JPanel {
         @Override
@@ -115,20 +154,33 @@ public class SelectionSort extends JFrame {
 
             if (array != null) {
                 double barWidth = getWidth() / (double) array.size();
+                double barMaxHeight = getHeight() - 30;
                 for (int i = 0; i < array.size(); ++i) {
-                    if (i < sortedIndex) {
+//                    if (i < sortedIndex) {
+                    if (i < array.size() / 3) {
                         g2.setColor(Color.cyan);
-                    } else if (i == currIndex) {
+//                    } else if (i == currIndex) {
+                    } else if (i == (array.size() * 2) / 3) {
                         g2.setColor(Color.pink);
-                    } else if (i == minIndex) {
+//                    } else if (i == minIndex) {
+                    } else if (i == array.size() / 2) {
                         g2.setColor(Color.red);
                     } else {
                         g2.setColor(Color.blue);
                     }
-                    double barHeight = (getHeight() * (array.get(i) / (double) array.size()));
-                    Shape r = new Rectangle2D.Double(i * barWidth, getHeight() - barHeight, barWidth, barHeight);
+                    double barHeight = (barMaxHeight * (array.get(i) / (double) array.size()));
+                    Shape r = new Rectangle2D.Double(i * barWidth, barMaxHeight - barHeight, barWidth, barHeight);
                     g2.fill(r);
                     g2.draw(r);
+
+                    g2.setColor(Color.black);
+                    String iStr = array.get(i).toString();
+                    FontMetrics fm = g2.getFontMetrics();
+                    float fx = (float) (((i + 1) * barWidth + i * barWidth) / 2.);
+                    float fy = (float) ((getHeight() + barMaxHeight) / 2.);
+                    fx -= fm.stringWidth(iStr) / 2.;
+                    fy += fm.getAscent() / 2.;
+                    g2.drawString(iStr, fx, fy);
                 }
             }
         }
